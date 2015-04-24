b36903161d17d155b39999a7284d68f2efb66390
Benji1 <Benji1@users.noreply.github.com>
2015-04-24 16:10:38 +0200

    forgot main

--- BasicGame/src/mygame/Main.java
+++ BasicGame/src/mygame/Main.java
@@ -19,15 +19,21 @@ import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.CameraNode;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.control.CameraControl;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
-import com.jme3.util.SkyFactory;
 import config.ConfigReader;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 import universe.Universe;
-
 
 /**
  * test
@@ -39,10 +45,9 @@ public class Main extends SimpleApplication implements ActionListener {
     private CameraNode camNode;
     public BasicShip s;
     private Universe u;
-    
+    private List<Body> bodies = new ArrayList<Body>();
     public BitmapText textShipPos;
     public BitmapText textNewChunk;
-    
     protected float shipSpeed = 0;
     protected float shipRotation = 1.5f;
     protected int rotDir = 0;
@@ -55,10 +60,11 @@ public class Main extends SimpleApplication implements ActionListener {
         settings.setResolution(1280, 720);
         settings.setVSync(true);
         settings.setBitsPerPixel(24);
-        settings.setSamples(32);
-        
+        settings.setSamples(16);
+
         Main app = new Main();
         app.setSettings(settings);
+        app.setShowSettings(false);
         app.start();
     }
 
@@ -72,32 +78,60 @@ public class Main extends SimpleApplication implements ActionListener {
         this.initCamera();
         this.initKeys();
         this.initHUD();
+        this.initPhysics();
     }
-    
+
     private void initShip() {
         s = new BasicShip(assetManager, this);
         rootNode.attachChild(s);
 
-        s.addModule(new Cockpit(), new Point(s.modules.length / 2, s.modules.length / 2));
-        s.addModule(new Armor(), new Point(s.modules.length / 2, s.modules.length / 2 - 1));
-        s.addModule(new Armor(), new Point(s.modules.length / 2, s.modules.length / 2 + 1));
+        Cockpit cockpit = new Cockpit();
+        s.addModule(cockpit, new Point(s.modules.length / 2, s.modules.length / 2));
+        cockpit.addToShip();
 
-        s.addModule(new Weapon(), new Point(s.modules.length / 2 - 2, s.modules.length / 2));
-        s.addModule(new Thruster(), new Point(s.modules.length / 2 + 2, s.modules.length / 2));
-        s.addModule(new Shield(), new Point(s.modules.length / 2 - 1, s.modules.length / 2));
+        Armor armor1 = new Armor();
+        s.addModule(armor1, new Point(s.modules.length / 2, s.modules.length / 2 - 1));
+        armor1.lockToShip();
+
+        Armor armor2 = new Armor();
+        s.addModule(armor2, new Point(s.modules.length / 2, s.modules.length / 2 + 1));
+        armor2.lockToShip();
+
+        Weapon weapon = new Weapon();
+        s.addModule(weapon, new Point(s.modules.length / 2 - 2, s.modules.length / 2));
+        weapon.lockToShip();
+
+        Thruster thruster = new Thruster(2);
+        s.addModule(thruster, new Point(s.modules.length / 2 + 2, s.modules.length / 2));
+        thruster.lockToShip();
+
+        Thruster thruster2 = new Thruster(1);
+        s.addModule(thruster2, new Point(s.modules.length / 2 + 1, s.modules.length / 2 + 1));
+        thruster2.lockToShip();
+
+        Thruster thruster3 = new Thruster(3);
+        s.addModule(thruster3, new Point(s.modules.length / 2 + 1, s.modules.length / 2 - 1));
+        thruster3.lockToShip();
+
+        Shield shield = new Shield();
+        s.addModule(shield, new Point(s.modules.length / 2 - 1, s.modules.length / 2));
+        shield.lockToShip();
 
         EnergyGenerator eg = new EnergyGenerator();
         s.addModule(eg, new Point(s.modules.length / 2 + 1, s.modules.length / 2));
+        eg.lockToShip();
 
-        EnergyGenerator eg2 = new EnergyGenerator();
-        s.addModule(eg2, new Point(s.modules.length / 2, s.modules.length / 2 + 2));
 
-        //s.print();
+        //EnergyGenerator eg2 = new EnergyGenerator();
+        //s.addModule(eg2, new Point(s.modules.length / 2, s.modules.length / 2 + 2));
+        //eg2.lockToShip();
+
+        s.print();
 
         //eg.printModules();
         //eg2.printModules();
     }
-    
+
     private void initCamera() {
         flyCam.setEnabled(false);
         stateManager.detach(stateManager.getState(FlyCamAppState.class));
@@ -108,30 +142,32 @@ public class Main extends SimpleApplication implements ActionListener {
         camNode.setLocalTranslation(new Vector3f(0, 70 * (this.viewPort.getCamera().getWidth() / 1280f), 0.1f));
         camNode.lookAt(this.s.getLocalTranslation(), Vector3f.UNIT_Y);
     }
-    
+
     private void initKeys() {
         inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_UP), new KeyTrigger(KeyInput.KEY_W));
         inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_LEFT), new KeyTrigger(KeyInput.KEY_A));
         inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_RIGHT), new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Weapon", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Shield", new KeyTrigger(KeyInput.KEY_F));
         inputManager.addMapping("ToggleUniverseDebug", new KeyTrigger(KeyInput.KEY_U));
-        
-        inputManager.addListener(this, "Up", "Left", "Right", "ToggleUniverseDebug");
+
+        inputManager.addListener(this, "Up", "Left", "Right", "Weapon", "Shield", "ToggleUniverseDebug");
     }
-    
+
     private void initHUD() {
-        this.textShipPos = new BitmapText(guiFont, false);          
+        this.textShipPos = new BitmapText(guiFont, false);
         this.textShipPos.setSize(guiFont.getCharSet().getRenderedSize());      // font size
         this.textShipPos.setColor(ColorRGBA.Green);                             // font color
         this.textShipPos.setText("POS");             // the text
         this.textShipPos.setLocalTranslation(0, this.settings.getHeight(), 0); // position
-        
-        this.textNewChunk = new BitmapText(guiFont, false);          
+
+        this.textNewChunk = new BitmapText(guiFont, false);
         this.textNewChunk.setSize(guiFont.getCharSet().getRenderedSize());      // font size
         this.textNewChunk.setColor(ColorRGBA.Green);                             // font color
         this.textNewChunk.setText("CHUNK UPDATES\n");             // the text
-        this.textNewChunk.setLocalTranslation(this.settings.getWidth() -250, this.settings.getHeight(), 0); // position
+        this.textNewChunk.setLocalTranslation(this.settings.getWidth() - 250, this.settings.getHeight(), 0); // position
     }
-    
+
     private void initWorld() {
         // testbox
         Box box1 = new Box(1, 1, 1);
@@ -151,33 +187,49 @@ public class Main extends SimpleApplication implements ActionListener {
             rootNode.attachChild(blue2);
         }
     }
-    
+
     private void initLight() {
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
         rootNode.addLight(sun);
     }
-    
+
     @Override
     public void onAction(String name, boolean keyPressed, float tpf) {
         if (name.equals("Up")) {
-            shipSpeed = 0.25f;
+            s.getInteractiveModule(new Point(s.modules.length / 2 + 2, s.modules.length / 2)).activate();
             if (!keyPressed) {
-                shipSpeed = 0f;
+                s.getInteractiveModule(new Point(s.modules.length / 2 + 2, s.modules.length / 2)).deactivate();
             }
         }
         if (name.equals("Left")) {
-            this.rotDir = 1;
+            s.getInteractiveModule(new Point(s.modules.length / 2 + 1, s.modules.length / 2 + -1)).activate();
             if (!keyPressed) {
-                this.rotDir = 0;
+                s.getInteractiveModule(new Point(s.modules.length / 2 + 1, s.modules.length / 2 + -1)).deactivate();
             }
         }
         if (name.equals("Right")) {
-            this.rotDir = -1;
+            s.getInteractiveModule(new Point(s.modules.length / 2 + 1, s.modules.length / 2 + 1)).activate();
             if (!keyPressed) {
-                this.rotDir = 0;
+                s.getInteractiveModule(new Point(s.modules.length / 2 + 1, s.modules.length / 2 + 1)).deactivate();
             }
         }
+
+        if (name.equals("Weapon")) {
+            s.getInteractiveModule(new Point(s.modules.length / 2 - 2, s.modules.length / 2)).activate();
+            if (!keyPressed) {
+                s.getInteractiveModule(new Point(s.modules.length / 2 - 2, s.modules.length / 2)).deactivate();
+            }
+        }
+
+        if (name.equals("Shield") && !keyPressed) {
+            if (s.getInteractiveModule(new Point(s.modules.length / 2 - 1, s.modules.length / 2)).isActive()) {
+                s.getInteractiveModule(new Point(s.modules.length / 2 - 1, s.modules.length / 2)).deactivate();
+            } else {
+                s.getInteractiveModule(new Point(s.modules.length / 2 - 1, s.modules.length / 2)).activate();
+            }
+        }
+        
         if (name.equals("ToggleUniverseDebug")) {
         	if (!keyPressed) {
 	        	if(camNode.getLocalTranslation().y == 70 * (this.viewPort.getCamera().getWidth() / 1280f)) {
@@ -195,26 +247,104 @@ public class Main extends SimpleApplication implements ActionListener {
         }
     }
 
-    
     @Override
-    public void simpleUpdate(float tpf) {
-    	this.u.update(tpf);
-        s.update(tpf);
-        
-        // update movement
-        Vector3f lookDir = this.s.getLocalRotation().mult(Vector3f.UNIT_Z).mult(-1).mult(shipSpeed).clone();
-        this.s.move(lookDir);
+    public void simpleUpdate(float delta) {
+        this.u.update(delta);
+        s.update(delta);
+
+        phyicsUpdate(delta);
+        // update movement        
+        //Vector3f lookDir = this.s.getLocalRotation().mult(Vector3f.UNIT_Z).mult(-1).mult(shipSpeed).clone();
+        //this.s.move(lookDir);
 
         // update rotation
-        this.s.rotate(0, tpf * speed * shipRotation * rotDir, 0);
+        //this.s.rotate(0, delta * speed * shipRotation * rotDir, 0);
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
-    
+
     public Universe getUniverse() {
         return this.u;
+    }
+    Body testBody;
+    Spatial testBox;
+
+    public void initPhysics() {
+        //PhysicsWorld.world.setGravity(new Vector2(0.0, -9.7));
+        //testBody = generateBody();
+        //testBox = generateCrapBox();
+    }
+
+    public Body generateBody() {
+        //Rectangle rect = new Rectangle(1, 1);                    
+        CircleShape circle = new CircleShape();
+        circle.m_radius = 1.0f;
+
+        FixtureDef fDef = new FixtureDef();
+        fDef.shape = circle;
+        fDef.density = 1.0f;
+        fDef.friction = 0.6f;
+        //fDef.restitution = 0.5f;
+
+        // set body                        
+        BodyDef bDef = new BodyDef();
+        bDef.position.set(0, -1);
+        bDef.type = BodyType.DYNAMIC;
+
+        Body body = PhysicsWorld.world.createBody(bDef);
+        body.createFixture(fDef);
+        //body.setLinearDamping(0.3);
+        //body.setMass(Mass.Type.NORMAL);
+
+
+        //PhysicsWorld.world.addBody(body);
+
+        //System.out.println("dynamic: " + body.isDynamic());
+
+        return body;
+    }
+
+    public Spatial generateCrapBox() {
+        Box box1 = new Box(1, 1, 1);
+        Geometry blue = new Geometry("Box", box1);
+        blue.setLocalTranslation(new Vector3f(0, -0, 0));
+        Material mat1 = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat1.setColor("Color", ColorRGBA.Blue);
+        blue.setMaterial(mat1);
+        rootNode.attachChild(blue);
+
+        return blue;
+    }
+
+    public Vector3f Vector2ToVector3f(Vec2 v2) {
+        Vector3f returnVec = new Vector3f();
+        returnVec.x = (float) v2.x;
+        returnVec.y = 0;
+        returnVec.z = (float) v2.y;
+        return returnVec;
+    }
+
+    public Vec2 Vector3fToVector2(Vector3f v3) {
+        Vec2 returnVec = new Vec2();
+        returnVec.x = v3.x;
+        returnVec.y = v3.z;
+        return returnVec;
+    }
+
+    public Vec2 getBodyPos(Body b) {
+        return b.getWorldPoint(b.getLocalCenter());
+    }
+
+    public void phyicsUpdate(float delta) {
+        //testBody.applyForce(new Vec2(-51f, 0f), testBody.getLocalCenter());
+
+        // Vector3f crapV2 = Vector2ToVector3f(getBodyPos(testBody));
+        //testBox.setLocalTranslation(crapV2);
+
+        PhysicsWorld.world.step(delta, 8, 8);
     }
 }
