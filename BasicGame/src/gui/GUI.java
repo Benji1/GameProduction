c2ff0cf9ce289e0fc0ad9546fa26b387db4d8c6a
Eva Brucker <evabrucker@aon.at>
2015-05-08 10:40:08 +0200

    gui parts panel generated in code

--- BasicGame/src/gui/GUI.java
+++ BasicGame/src/gui/GUI.java
@@ -60,7 +60,48 @@ public class GUI {
     }
     
     private void setupPartsPanel() {
+        Element partsPanel = editorScreen.findElementByName("parts-panel");
         
+        // should be able to get this info from the inventory later on (also sprite img id)
+        int numOfDifferentItems = 6;
+        int[] numOfEachItem = new int[] {1, 8, 3, 20, 14, 8};        
+        
+        int numOfCols = 4;
+        int numOfBgPanels = (int) (Math.ceil(numOfDifferentItems/(numOfCols*1f))) * numOfCols;
+        final int gridItemSize = 100;  
+        int gridOffset = 10;
+        
+        ControlBuilder partPanel = new ControlBuilder("part-panel") {{
+            parameter("width", gridItemSize+"px");
+            parameter("height", gridItemSize+"px");
+        }};
+        ControlBuilder emptyPanel = new ControlBuilder("empty-slot") {{
+        }};
+        
+        for (int i=0; i<numOfBgPanels; i++) {
+            int x = gridOffset * (i%numOfCols+1) + gridItemSize * (i%numOfCols);
+            int y = gridOffset * (i/numOfCols+1) + gridItemSize * (i/numOfCols);
+            
+            if (i < numOfDifferentItems) {
+                int tileImgId = (i%numOfDifferentItems)+2;
+                String numOfItems;
+                
+                if (numOfEachItem[i] < 10)
+                    numOfItems = "x0"+numOfEachItem[i];
+                else 
+                    numOfItems = "x"+numOfEachItem[i];
+                
+                partPanel.parameter("label", numOfItems);
+                partPanel.parameter("x", x+"px");
+                partPanel.parameter("y", y+"px");
+                partPanel.parameter("sprite", "sprite:100,100,"+tileImgId);
+                partPanel.build(nifty, editorScreen, partsPanel);
+            } else {
+                emptyPanel.parameter("x", x+"px");
+                emptyPanel.parameter("y", y+"px");
+                emptyPanel.build(nifty, editorScreen, partsPanel);
+            }
+        }
     }
     
 }
