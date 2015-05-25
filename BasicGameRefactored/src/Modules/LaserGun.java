5ed9e975613f94edbc898e07470dd9bc02c02601
Benji1 <Benji1@users.noreply.github.com>
2015-05-25 14:27:08 +0200

    sound for projectile

--- BasicGameRefactored/src/Modules/LaserGun.java
+++ BasicGameRefactored/src/Modules/LaserGun.java
@@ -4,7 +4,9 @@
  */
 package Modules;
 
+import com.jme3.audio.AudioNode;
 import java.util.ArrayList;
+import mygame.BasicShip;
 import org.jbox2d.common.Vec2;
 import weapons.LaserProjectile;
 
@@ -13,6 +15,8 @@ import weapons.LaserProjectile;
  * @author Eva
  */
 public class LaserGun extends Weapon {
+    
+    private AudioNode fire_sound;
 
     public LaserGun(ArrayList<String> hotkeys, FacingDirection orientationDirection) {
         super(hotkeys, orientationDirection);
@@ -26,6 +30,17 @@ public class LaserGun extends Weapon {
     protected void fire() {
         Vec2 fireDirection = body.getWorldVector(orientation);
         LaserProjectile p = new LaserProjectile(body.getWorldPoint(body.getLocalCenter()).add(new Vec2(2f * fireDirection.x, 2f * fireDirection.y)), fireDirection, ship.getApp());
+        fire_sound.playInstance();
+    }
+    
+    @Override
+    public void onPlaced (BasicShip ship) {
+        super.onPlaced(ship);
+        fire_sound = new AudioNode(ship.getApp().getAssetManager(), "Sound/Effects/LaserProjectile.ogg", false);
+        fire_sound.setPositional(false);
+        fire_sound.setLooping(false);
+        fire_sound.setVolume(1);
+        this.attachChild(fire_sound);
     }
     
     @Override
