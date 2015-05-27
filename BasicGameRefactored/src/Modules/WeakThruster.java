f874e95385dd5069561cdc6fb72382f6676b73b3
Benji1 <Benji1@users.noreply.github.com>
2015-05-27 19:15:40 +0200

    why not extend weakthruster from thruster?

--- BasicGameRefactored/src/Modules/WeakThruster.java
+++ BasicGameRefactored/src/Modules/WeakThruster.java
@@ -4,41 +4,16 @@
  */
 package Modules;
 
-import com.jme3.math.ColorRGBA;
 import java.util.ArrayList;
-import org.jbox2d.common.Vec2;
 
 /**
  *
  * @author 1337
  */
-public class WeakThruster extends InteractiveModule {
-
-    protected float forceMagnitude = cr.getFromMap(cr.getBaseMap("Thruster"), "ForceMagnitude", float.class);
-    protected float linearDampingFactor = cr.getFromMap(cr.getBaseMap("Thruster"), "LinearDampingWeak", float.class);
-    protected Vec2 orientation;
+public class WeakThruster extends Thruster {
 
     public WeakThruster(ArrayList<String> hotkeys, FacingDirection orientationDirection) {
-        super(hotkeys);
-        moduleName = "Thruster";
-        energyConsumptionPerSecond = cr.getFromMap(cr.getBaseMap("Thruster"), "EnergyConsumptionPerSecond", float.class);
-
-        color = ColorRGBA.LightGray;
-        colorActive = ColorRGBA.Orange;
-
-        orientation = FacingDirection.getDirectionVector(orientationDirection);
-    }
-
-    protected void onActive() {
-        Vec2 forceDirection = body.getWorldVector(orientation).mul(forceMagnitude);
-        body.applyForce(forceDirection, body.getPosition());
-    }
-   
-    @Override
-    public void update(float delta) {
-        super.update(delta);
-        if (!active) {
-            this.body.setLinearDamping(linearDampingFactor);
-        }
+        super(hotkeys, orientationDirection);
+        moduleName = "WeakThruster";
     }
 }
