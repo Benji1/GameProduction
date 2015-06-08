3cb90f4cccf344186a8589aa893f7f04471a6e5d
Areen Said <areen@live.at>
2015-06-08 15:35:44 +0200

    thruster particles added (not final)

--- BasicGameRefactored/src/Modules/Thruster.java
+++ BasicGameRefactored/src/Modules/Thruster.java
@@ -5,8 +5,11 @@
 package Modules;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.texture.Texture;
 import gui.ModuleType;
 import java.util.ArrayList;
@@ -19,6 +22,7 @@ import org.jbox2d.common.Vec2;
 public class Thruster extends InteractiveModule {
 
     protected float forceMagnitude = cr.getFromMap(cr.getBaseMap("Thruster"), "ForceMagnitude", float.class);
+    ParticleEmitter fire;
 
     public Thruster(ArrayList<String> hotkeys, FacingDirection orientationDirection) {
         super(hotkeys);
@@ -40,7 +44,22 @@ public class Thruster extends InteractiveModule {
     @Override
     public void update(float delta) {
         super.update(delta);
+        fire.setLocalTranslation(this.body.getPosition().x, 0, this.body.getPosition().y);
     }
+    
+    @Override
+    public void activate() {
+        super.activate();
+        fire.emitAllParticles();
+    }
+
+    @Override
+    public void deactivate() {
+        super.deactivate();
+        fire.killAllParticles();
+    }
+
+
     
     @Override
     protected void create3DBody() {
@@ -53,5 +72,22 @@ public class Thruster extends InteractiveModule {
         spatial.setMaterial(material);
         
         materialActive.setTexture("DiffuseMap", t);
+        
+        fire = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 30);
+        Material mat_red = new Material(a, "Common/MatDefs/Misc/Particle.j3md");
+        mat_red.setTexture("Texture", a.loadTexture("textures/flame.jpg"));
+        fire.setMaterial(mat_red);        
+        fire.setImagesX(2); fire.setImagesY(2); // 2x2 texture animation
+        fire.setEndColor(  new ColorRGBA(1f, 0f, 0f, 1f));   // red
+        fire.setStartColor(new ColorRGBA(1f, 1f, 0f, 0.5f)); // yellow
+        fire.getParticleInfluencer().setInitialVelocity(new Vector3f(0,2,0));
+        fire.setStartSize(1.5f);
+        fire.setEndSize(0.1f);
+        fire.setGravity(0,0,0);
+        fire.setLowLife(0.5f);
+        fire.setHighLife(3f);
+        fire.getParticleInfluencer().setVelocityVariation(0.3f);
+        ship.getApp().getRootNode().attachChild(fire);
+        //System.out.println(this.);
     }
 }
